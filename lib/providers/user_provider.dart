5c2a61146987c4584b74bb57cce4772914cf89f9
PartyDonut <42371342+PartyDonut@users.noreply.github.com>
2026-08-28 12:05:32 +0200

    fix: Properly save localUrl changes
    (cherry picked from commit e3f325b8a7c086007f6e0a71bd2b340110c13a51)

--- lib/providers/user_provider.dart
+++ lib/providers/user_provider.dart
@@ -204,6 +204,7 @@ class User extends _$User {
     state = user.copyWith(
       credentials: user.credentials.copyWith(localUrl: value?.isEmpty == true ? null : value),
     );
+    userState = state;
   }
 
   void setSeerrServerUrl(String? value) {
